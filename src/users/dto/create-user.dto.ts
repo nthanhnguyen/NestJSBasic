f670ec9dc9ac91e855e30b7204805eeb679041ab
NT NGUYÊN <ntnguyen10222002@gmail.com>
2024-01-04 19:42:21 +0700

    CRUD user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+    @IsEmail({}, {
+        message: 'Email ko đúng định dạng',
+    })
+    @IsNotEmpty({
+        message: 'Email ko đc để trống',
+    })
     email: string;
+
+    @IsNotEmpty({
+        message: 'Password ko đc để trống',
+    })
     password: string;
+
     name: string;
     address: string
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { genSaltSync, hashSync } from 'bcryptjs';
 @Injectable()
 export class UsersService {
@@ -32,15 +32,19 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return 'Not found User'
+    return this.userModel.findOne({ _id: id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(updateUserDto: UpdateUserDto) {
+    return await this.userModel.updateOne({ _id: updateUserDto._id }, { ...updateUserDto });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return 'Not found User'
+    return this.userModel.deleteOne({ _id: id });
   }
 }
