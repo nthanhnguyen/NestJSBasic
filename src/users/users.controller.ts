325238ac13037378305324243bca021d66c38b13
NT NGUYÊN <ntnguyen10222002@gmail.com>
2024-01-04 16:13:56 +0700

    #22.3 DTO - Data Transfer Object

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,15 +9,10 @@ export class UsersController {
 
   @Post()
   create(
-    @Body("email") email: string,
-    @Body("password") password: string,
-    @Body("name") name: string
+    @Body() createUserDto: CreateUserDto
   ) {
-    return this.usersService.create(email, password, name);
+    return this.usersService.create(createUserDto);
   }
-  // create(@Body() createUserDto: CreateUserDto) {
-  //   return this.usersService.create(createUserDto);
-  // }
 
   @Get()
   findAll() {
