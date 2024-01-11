a787df12b45b8dfa466b152594d32c49a9be9c4b
NT NGUYÊN <ntnguyen10222002@gmail.com>
2024-01-11 21:04:31 +0700

    Setup CORS NestJS

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const reflector = app.get(Reflector);
-  app.useGlobalGuards(new JwtAuthGuard(reflector)); //
+  //app.useGlobalGuards(new JwtAuthGuard(reflector)); //
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
@@ -20,6 +20,15 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
+  //config cors
+  app.enableCors(
+    {
+      "origin": "*",
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "preflightContinue": false,
+      "optionsSuccessStatus": 204
+    }
+  );
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
