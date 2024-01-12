62a0fabc333ac451faf59d76e28284323ec0f1a6
NT NGUYÊN <ntnguyen10222002@gmail.com>
2024-01-12 18:18:26 +0700

    Generate Modules Company

---
+++ src/companies/dto/create-company.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateCompanyDto {
+    @IsNotEmpty({
+        message: 'Name ko đc để trống',
+    })
+    name: string;
+
+    @IsNotEmpty({
+        message: 'address ko đc để trống',
+    })
+    address: string;
+
+    @IsNotEmpty({
+        message: 'Description ko đc để trống',
+    })
+    description: string;
+
+
+}
