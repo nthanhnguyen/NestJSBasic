22ca6af5bafd6946f19ea4803986f2a56f7cba42
NT NGUYÊN <ntnguyen10222002@gmail.com>
2024-01-13 16:21:21 +0700

    Passing Req.user

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,13 +4,20 @@ import { UpdateCompanyDto } from './dto/update-company.dto';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { Company, CompanyDocument } from './shemas/company.shema';
 import { InjectModel } from '@nestjs/mongoose';
+import { IUser } from 'src/users/user.interface';
 @Injectable()
 export class CompaniesService {
 
   constructor(@InjectModel(Company.name) private companyModel: SoftDeleteModel<CompanyDocument>) { }
 
-  create(createCompanyDto: CreateCompanyDto) {
-    return this.companyModel.create({ ...createCompanyDto })
+  create(createCompanyDto: CreateCompanyDto, user: IUser) {
+    return this.companyModel.create({
+      ...createCompanyDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email
+      }
+    })
     // let company = await this.userModel.create({
     //   name: createCompanyDto.name,
     //   address: createCompanyDto.address,
