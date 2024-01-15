d96e7f85e4b3e0a5b28b4562ec5e3a377269b379
NT NGUYÊN <ntnguyen10222002@gmail.com>
2024-01-15 15:37:46 +0700

    Query with Pagination

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -5,6 +5,9 @@ import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { Company, CompanyDocument } from './shemas/company.shema';
 import { InjectModel } from '@nestjs/mongoose';
 import { IUser } from 'src/users/user.interface';
+import mongoose from 'mongoose';
+import aqp from 'api-query-params';
+
 @Injectable()
 export class CompaniesService {
 
@@ -26,8 +29,32 @@ export class CompaniesService {
     // return company;
   }
 
-  findAll() {
-    return `This action returns all companies`;
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const { filter, sort, population } = aqp(qs);
+    delete filter.page;
+    delete filter.limit;
+    let offset = (+currentPage - 1) * (+limit);
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.companyModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.companyModel.find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems // tổng số phần tử (số bản ghi)
+      },
+      result //kết quả query
+    }
   }
 
   findOne(id: number) {
@@ -45,7 +72,14 @@ export class CompaniesService {
       });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
+  async remove(id: string, user: IUser) {
+    await this.companyModel.updateOne({ _id: id },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email
+        }
+      });
+    return this.companyModel.softDelete({ _id: id });
   }
 }
