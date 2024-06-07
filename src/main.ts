605bab78d8481c968ed867c988a2573fc20a0b5d
NT NGUYÊN <ntnguyen10222002@gmail.com>
2024-06-07 22:20:44 +0700

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+  //const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
 
   const reflector = app.get(Reflector);
